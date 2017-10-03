7a2a1e01fa2f3977c23a0b8bd689b08db9ea6c1a
Brandon  Harris <cheeridon@gmail.com>
2017-10-03 19:55:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday} #{supplies.join(", ")}"
+      puts " #{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end
 end
